7ce45607d78dcb3a51353f1c507e652bfbaf3eca
selena <selena.fahet.kestis@gmail.com>
2024-02-25 10:40:50 +0100

    récupère les champs input sans insertion

--- src/Controller/AjoutController.php
+++ src/Controller/AjoutController.php
@@ -2,20 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Date;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Entity\ORM;
 
 class AjoutController extends AbstractController
 {
     /**
      * @Route("/Ajout", name="Ajout")
      */
-    public function index(): Response
+    public function index()
     {
         return $this->render('Ajout/Ajout.html.twig');
     }
@@ -30,27 +27,12 @@ class AjoutController extends AbstractController
         $nom = $request->request->get('nom');
         $prenom = $request->request->get('prenom');
 
-        // Créer une nouvelle instance de l'entité Date et définir ses propriétés
-        $entity = new Date(); // Utilisation de l'entité Date
-        $entity->setDate(new \DateTime($date)); // Assurez-vous que votre propriété date est de type \DateTime
-        $entity->setNom($nom);
-        $entity->setPrenom($prenom);
-
-        // Obtenir le gestionnaire d'entités (Entity Manager)
-        $entityManager = $this->getDoctrine()->getManager();
-
-        // Persistez l'entité
-        $entityManager->persist($entity);
-
-        // Exécuter les opérations en base de données
-        $entityManager->flush();
-
         // Création de la réponse JSON
         $response = new JsonResponse([
-            'message' => 'Données insérées avec succès',
-            'date_ajoutee' => $date,
-            'nom_ajoutee' => $nom,
-            'prenom_ajoutee' => $prenom,
+            'message' => 'Données reçues avec succès',
+            'date' => $date,
+            'nom' => $nom,
+            'prenom' => $prenom,
         ]);
 
         return $response;
